4ff499e04cf1ae9732d991638a2a0857190a0d65
BuildTools <unconfigured@null.spigotmc.org>
2021-09-10 20:56:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ptsganjil202111rpl2rizqieahmadzakariahende26/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ptsganjil202111rpl2rizqieahmadzakariahende26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
